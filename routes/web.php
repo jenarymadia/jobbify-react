61733706aa25d019232b3518714c75587f3b7ebf
Jenary Madia <jenary.madia@rebisco.com.ph>
2024-10-26 02:25:26 +0800

    add all indexes of needed routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\LeadController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,7 +31,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::resource('clients', ClientController::class)->names('clients');
+    Route::resource('leads', LeadController::class)->names('leads');
+    Route::resource('jobs', JobController::class)->names('jobs');
     Route::resource('quotes', QuoteController::class)->names('quotes');
+    Route::resource('invoices', InvoiceController::class)->names('invoices');
+    Route::resource('staffs', StaffController::class)->names('staffs');
+    Route::resource('services', ServiceController::class)->names('services');
+    Route::resource('schedule', ScheduleController::class)->names('schedule');
 });
 
 require __DIR__.'/auth.php';
